03a6ec055128c2ce381c492686fdc67165fff294
AdonNeet <baradontap@gmail.com>
2023-12-14 11:01:25 +0700

    [hajimete] First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pemiltum Data Importer
+
+Automation script to import the holders of the right to vote data to MySQL database.
+
+## How to use
+
+1. Install required packages
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Install Python MySQL Connector
+
+```bash
+python -m pip install mysql-connector-python
+```
+
+> Or download the package from their official website to perform manual installation <br> <u>[https://downloads.mysql.com/archives/c-python/](https://downloads.mysql.com/archives/c-python/) </u>
+
+3. Execute command
+
+```bash
+python importer.py
+```
---
+++ importer.py
@@ -0,0 +1,54 @@
+import mysql.connector
+import pandas as pd
+from text import TextColor as tc
+
+HOST = "localhost"
+USER = "root"
+PASSWORD = ""
+DATABASE = "web_pemilu_fosti"
+EXCEL_FILE = "data.xlsx"
+
+def connect_mysql(host, user, password, database):
+    db = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    return db
+
+def insert_participant(email):
+    cursor = db.cursor()
+    sql = "INSERT INTO emails (email, created_at, updated_at) VALUES (%s, NOW(), NOW())"
+    cursor.execute(sql, email)
+    db.commit()
+    cursor.close()
+
+if __name__ == "__main__":
+    try:
+        db = connect_mysql(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE
+        )
+        print(f"{tc.GREEN}Success: Connected to database{tc.END}")
+    except:
+        print(f"{tc.RED}Error: Can't connect to database{tc.END}")
+        exit(1)
+
+    df = pd.read_excel(EXCEL_FILE)
+    participants = tuple(df[['name', 'student_id', 'email']].values)
+    for name, student_id, email in participants:
+        try:
+            student_id = student_id.upper()
+            name = name.title()
+            email = email.replace(" ", "")
+            print(f"{tc.YELLOW}Action: Inserting {student_id} {name}{tc.END}")
+            insert_participant(email)
+            print(f"{tc.GREEN}Success: Inserting {student_id} {name}{tc.END}")
+        except:
+            print(f"{tc.RED}Error: Inserting {student_id} {name}{tc.END}")
+            continue
+    
+    db.close()
